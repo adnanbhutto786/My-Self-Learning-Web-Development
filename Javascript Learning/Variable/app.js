6de72c13f5c0bda9d16542fe7c73ed7853814b40
Adnan Ali Bhutto <adnan2292707@gmail.com>
2025-09-28 15:14:57 +0500

    javascript
    Variable in string and number complete all data types

---
+++ Javascript Learning/Variable/app.js
@@ -0,0 +1,88 @@
+// console.log("Hello Word");
+// console.log("My Name is Adnan Ali Bhutto");
+// console.log("sum",21+34);
+
+//  let name = "Adnan"
+//  console.log(name);
+
+//  let fatherName = "Raza Muhammad"
+//  console.log(fatherName);
+
+//  let userName = "Adnan Ali Bhutto"
+//  console.log(userName);
+  
+// let password = "admin123"
+// console.log("Password:",password);
+
+// let city = "Karachi"
+// console.log(city);
+
+// let country = "Pakistan"
+// console.log(country);
+
+// variable declare only 3 process
+//   1. var it is old js version recenlty not used and its not perfect answer....
+
+    var myname = "ali"
+    console.log(myname);
+    
+//   2. let    block scop its curly bracket in.
+    //        mostly usecase change in value like jesa price
+
+    // let price = 45
+    // console.log(price);
+
+    let age = 12
+    console.log(age);
+    
+    
+
+    let myName = "Adnan Ali Bhutto"
+    console.log(myName);
+    
+//   3. const    block scope hai assign the value after not change its fixed value  like name email and pi value
+const pi = 3.14
+console.log(pi);
+
+
+let product = "Coffee";
+let price = "500 PKR";
+console.log(`The ${product} costs ${price}.`);
+
+let firstName = "Adnan"
+let lastName = "Ali"
+console.log(`${firstName} ${lastName}`);
+
+let product_first = "green tea"
+let product_price = "$3450"
+console.log(`The ${product_first} cost ${product_price}`);
+
+
+
+let a = "Hello"
+let b ="world"
+console.log(a+ " " + b);
+
+let text = "Javascript"
+console.log(text.length);
+
+
+let myFullName ="Adnan Ali Bhutto"
+let courseName ="Javascript"
+console.log("My name is " + myFullName + " and I am learning" + courseName + ".");
+
+
+
+//now variable in number ..................
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
---
+++ Javascript Learning/Variable/variableNumber.js
@@ -0,0 +1,39 @@
+console.log("Hello World");
+
+let age =23
+// let price = 12.34
+const pi = 3.14159
+// console.log(age , price, pi);
+
+let a = 12
+let b = 4
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a %  b);
+
+
+let myAge = 25 
+console.log("I am " + myAge + " Year old. and I am learning Web devepolment ");
+
+
+let score = 10
+score = score + 3
+console.log(score);
+
+let price = 40
+let tax =29
+let total = price + tax
+console.log("Total Bills:"+total);
+
+
+
+
+
+let apples = 10
+let mangoes = 4
+console.log("Total  fruites: " + (apples+mangoes));
+console.log("Differnce: " + (apples-mangoes));
+console.log("Multiplication: " + (apples*mangoes));
+console.log("product: " + (apples*mangoes));
