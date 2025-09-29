7e0256cc080015385d608109f0c950d83e63489c
Adnan Ali Bhutto <adnan2292707@gmail.com>
2025-09-29 10:07:05 +0500

    javascript
    Complete increment and decrement opreator in js

---
+++ Javascript Learning/Variable/Mini_project/Opeartor.js
@@ -0,0 +1,75 @@
+console.log("Hello Opeartor");
+
+let a = 25;
+let b =5;
+
+console.log("Addition", (a+b));
+console.log("Addition", (a-b));
+console.log("Addition", (a*b));
+console.log("Addition", (a/b));
+console.log("Addition", (a%b));
+
+//// unfamilar opartor 
+//increment opreator
+
+// let  x = 7 
+// x++;
+// console.log(x);
+
+// var num = 1;
+// var nuM2 = num++;
+// console.log(nuM2);
+ 
+// 1. post increment pahlaa value use kro phr +1 add hoga
+let x = 5;
+console.log(x++); // Output: 5 (pehle purani value use ki)
+console.log(x);   // Output: 6 (ab +1 ho gaya)
+
+// pre incremenet 
+
+let t = 9 ;
+console.log(++t);
+console.log(t);
+
+
+
+// post decrement 
+
+let z = 7;
+console.log(z--);
+console.log(z);
+
+
+
+//pre decrement
+let p = 34
+console.log(--p);
+console.log(p);
+
+
+//post increment
+let count = 0
+console.log(count++);
+console.log(count);
+
+//pre incrment
+console.log(++count);
+
+// post decremtn
+console.log(count--);
+console.log(count);
+
+console.log(--count);
+
+
+
+
+
+
+
+
+
+
+
+
+
