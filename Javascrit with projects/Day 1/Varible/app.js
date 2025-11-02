5491949de888f6da489c32c3ff9a37ace9dad278
Adnan Ali Bhutto <adnan2292707@gmail.com>
2025-11-02 13:36:47 +0500

    Add basic variable operations project (Day 1)
    Added initial files for a JavaScript project demonstrating variable usage and arithmetic operations. Includes app.js with sample variable declarations and operations, index.html to load the script, and a basic CSS reset in style.css.

---
+++ Javascrit with projects/Day 1/Varible/app.js
@@ -0,0 +1,68 @@
+// console.log("Hello world");
+
+// let num_1 = 34;
+// let num_2 = 45
+
+// console.log("Addition" , ( num_1 + num_2 ));
+// console.log("Subtraction" , ( num_1 - num_2 ));
+// console.log("product" ,  ( num_1 * num_2 ));
+// console.log("division" , ( num_1 / num_2 )); 
+// console.log("moduls" , ( num_1 % num_2 ));
+
+
+// alert("Hello how are you")
+
+// var name = "Adnan"
+// alert(name)
+
+var city = "Karachi"
+var country = "Pakistan"
+console.log(city);
+console.log(country);
+
+//  Combine multiple string
+
+// let firstName ="Adnan"
+// let lastName  ="Ali Bhutto"
+
+// console.log(firstName+ " " + lastName);
+
+
+//Variable for number
+
+// var num_1 = "5"
+// var num_2 = 5
+// console.log(num_1 + num_2);  55output both concatation
+var amount =1900
+var price = 700;
+var tax  = 300
+var total = price + tax;
+var Remaning = amount - total;
+console.log("Total price:", total);
+console.log("Remaning price:", Remaning);
+
+
+
+var x = 21;
+var y = 4;
+
+console.log("Addition:", (x +y));
+console.log("Subtraction: " + (x - y));
+console.log("Multiplication: " + (x * y));
+console.log("Division: " + (x / y));
+console.log("moduls:" ,(x % y) );
+
+
+var ticketPrice = 5000;
+var quantity = 3 ;
+var totalCost = ticketPrice * quantity
+console.log("The total ticket price is: "+ totalCost + " " + "PKR");
+
+
+
+
+
+
+
+
+
