4f141cc1025d71bd22726ad6b117207a81f9f3b0
Adnan Ali Bhutto <adnan2292707@gmail.com>
2025-11-03 14:40:10 +0500

    Add Day 2 JavaScript practice files
    Added app.js with examples of increment/decrement, operator precedence, string concatenation, template literals, and prompt usage. Added index.html as the entry point for the project.

---
+++ Javascrit with projects/Day 2/app.js
@@ -0,0 +1,199 @@
+// console.log("Hello world");
+
+
+// var a = 4 
+// console.log(a++);
+// console.log(a);
+
+// var b = 7
+// console.log(++b);
+// console.log(b);
+
+
+
+// var c = 3
+// console.log(c--);
+// console.log(c);
+
+
+// var c = 9
+// console.log(--c);
+// console.log(c);
+var counter = 0;
+
+// console.log("User visits:", ++counter); // 1
+// console.log("User visits:", --counter); // 0
+// console.log("User visits:", ++counter); // 1
+// console.log("User visits:", ++counter); // 2
+// console.log("User visits:", ++counter); // 2
+// console.log("User visits:", ++counter); // 2
+// console.log("User visits:", --counter); // 2
+
+// var counter = 1
+// console.log("User visits:", counter++); // 1
+// console.log("User visits:", counter--); // 0
+// console.log("User visits:", ++counter); // 1
+// console.log("User visits:", --counter); // 2
+// console.log("User visits:", ++counter); // 2
+// console.log("User visits:", ++counter); // 2
+// console.log("User visits:", --counter); // 2
+
+/// chapter 7 start 
+
+
+// var total = 2 + 3 * 4;
+// console.log(total);
+
+// // P E M D A S
+
+// // P → Parentheses ()
+// // E → Exponents (not common in JS directly)
+// // M → Multiplication *
+// // D → Division /
+// // A → Addition +
+// // S → Subtraction -
+
+// var result = (2 + 3) * 4;
+// console.log(result); // 20
+
+
+// var total = (2 + (3 * 4)) - (5 * (2 + 1));
+// console.log(total); // (2 + 12) - (5 * 3) = 14 - 15 = -1
+
+
+// var t =(10 + 5) * 2 - 8 / 4 + (6 * 2)
+// console.log(t);
+
+// var result = 2 ** 3;
+// console.log(result); // Output: 8'
+
+
+// var cube = 9 ** 3
+// console.log(cube);
+
+// var resultf = Math.pow(2, 3)
+// console.log(resultf);
+
+
+
+// var resultx = 2 + 3 ** 2 * (5 - 2);
+// console.log(resultx);
+
+
+// concatination string chapter 09
+
+
+var firstName = "Adnan";
+var lastName = "Bhutto";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+
+// + sign no ko combine krnaa ka liua be use hotaa haii
+
+
+var greeting = "Hello, " + "World!";
+console.log(greeting);
+
+
+var personal = "adnan"
+console.log("welcome, " +personal +  "!");
+
+// Buy Serum from Khuwaab only at Rs.799! 
+
+
+var brand = "khuwab"
+var  product = "serum"
+var price = 765;
+console.log("Buy " +product +" from " +brand + " only at " + "Rs."+799+"!");
+
+
+// ES6 BESt feature
+
+var nam = "adnan"
+var age = 21 ;
+console.log(`My name is ${nam} and i am ${age} year old`);
+
+// Hello [YourName]! You are [YourAge] years old and you live in [YourCity].
+
+
+// var nameee = "Adnan"
+// var ageee = 23; 
+// var city = "Pakistan"; 
+// console.log(`Hello ${nameee}! You are ${ageee} years and you live in ${city}`);
+
+
+// Chapter 09 prompt
+
+// var username =prompt("What is your name");
+// console.log("Hello " + username + "!");
+
+//Hi Adnan! You are 25 years old.
+
+// var myname = prompt("What is your name ?")
+// var myAge  = prompt("Enter your age ?")
+
+// console.log(`Hi ${myname}! You are ${myAge} years old.`);
+
+
+// var num1 =prompt("Enter first number");
+// var num2 =prompt("Enter second number")
+// var sum = Number (num1) + Number (num2)
+// var sub = Number (num1) - Number (num2)
+// var pro = Number (num1) * Number (num2)
+// var div = Number (num1) / Number (num2)
+// var mode = Number (num1) % Number (num2)
+// var power = Number (num1) ** Number (num2)
+// console.log("The sum of two number is: " + sum);
+// console.log("The sub of two number is: " + sub);
+// console.log("The pro of two number is: " + pro);
+// console.log("The div of two number is: " + div); 
+// console.log("The mode of two number is: " + mode); 
+// console.log("The power of two number is: " + power); 
+
+
+
+// var birthYear = prompt("Enter your birth Month")
+// var currentYear = 2025;
+
+// var age = Number (currentYear) - Number (birthYear) ;
+// console.log(`Your are ${age} old.`);
+
+
+//Hello [Name]! Your favorite color is [Color] and you love [Hobby].
+
+
+var myname = prompt("What is your name ?");
+var favoriteColor =prompt("what is your favrioute color");
+var myHobby = prompt("what is your hobby");
+
+
+console.log(`Hello ${myname}! Your favorite color is ${favoriteColor} and you love ${myHobby}`);
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
