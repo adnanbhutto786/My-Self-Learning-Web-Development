0b65f91ab56a606977060075449e4a735ca06ab7
Adnan Ali Bhutto <adnan2292707@gmail.com>
2025-11-03 12:45:21 +0500

    Add examples for operators and percentage calculation
    Added code snippets demonstrating arithmetic operations, pre/post increment and decrement operators, and a percentage calculation for a report card. Also included additional console log statements for practice and clarification.

--- Javascrit with projects/Day 1/Varible/app.js
+++ Javascrit with projects/Day 1/Varible/app.js
@@ -57,6 +57,107 @@ var ticketPrice = 5000;
 var quantity = 3 ;
 var totalCost = ticketPrice * quantity
 console.log("The total ticket price is: "+ totalCost + " " + "PKR");
+console.log("Pakistan zaindabad");
+
+
+console.log(2+23);
+
+// var userName = "Adnan";
+// var userAge = 25;
+// var $city = "Larkana";
+// var _country = "Pakistan";
+
+// console.log(userName);
+// console.log(userAge);
+// console.log($city);
+// console.log(_country);
+
+var math = 80;
+var eng = 90;
+var computer = 67;
+var urdu = 97;
+var obtain_marks = math + eng + computer + urdu;
+var totalMarks = 400;
+var percentage = obtain_marks/totalMarks * 100;
+console.log("Obtain_Marks", obtain_marks);
+console.log("The Percentage of this report card: "+ percentage.toFixed(2) + "%");
+
+
+/// unfamilar opeartor
+
+var a = 4
+a++;
+console.log(a);   //output 5
+
+// var b = 8
+// b--;
+// console.log(b);  7
+
+
+
+// pre and post increment and decrement
+
+// var z = 9 ;
+
+// console.log("orignal value:" , z);
+// console.log("post increment:" , z++);
+
+
+
+/// post increment
+// pahla old value then phr increment
+
+var a = 6;
+console.log(a++);
+console.log(a);
+
+
+/// pre increment pahla increment then pher addd
+var b = 9 ;
+console.log(++b);
+console.log(b);
+
+var x = 56;
+var y = x++ + 3;
+console.log("x:", x); // 6
+console.log("y:", y); // 8
+
+var u = 7
+var e = ++u + 4
+console.log("u" , u);    //8
+console.log("e" , e);    // 12
+
+
+var n = 2;
+console.log(n++ + ++n + n++); 
+
+
+var z = 10;
+console.log(z--); // 10 (print, phir 9)
+console.log(--z); // 8 (pehle 1 kam, phir print)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
