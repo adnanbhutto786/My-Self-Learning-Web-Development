68a511d2def7d4d8919c898a7dfb726952d08df5
Adnan Ali Bhutto <adnan2292707@gmail.com>
2025-11-04 07:59:59 +0500

    Add if-else statement examples and theme check script
    Added script.js with commented examples of if-else statements, comparison operators, and user input handling. Implemented a theme check that logs 'Light mode activated' if the theme variable equals 1. Added an empty style.css file for future styling.

---
+++ Javascrit with projects/Day 2/script.js
@@ -0,0 +1,83 @@
+// console.log("Hello world");
+
+/// chapter 10 start if else statement
+
+
+
+
+// var age = 10; 
+ 
+// if (age <=18){
+
+//    console.log("You go for picnic");
+   
+// }
+// var temperature = 45 
+// if (temperature > 30){
+//    console.log("today hot day");
+   
+
+// }
+// Write a small program that:
+
+// Takes user input of marks using prompt()
+
+// If marks >= 50 → print “You passed!”
+
+// Else → print “You failed!”
+
+//  var marks = prompt("Enter your marks");
+//  if ( marks >=50){
+//    console.log("You passed!");
+    
+//  } else {
+//    console.log("You failed!");
+   
+//  }
+
+
+// comparison  operator
+
+// console.log(5 == "5");    // true (value same)
+// console.log(5 === "5");   // false (type different)   campare  value and type both
+// console.log(8 != 10);     // true
+// console.log(10 !== "10"); // true
+// console.log(7 > 3);       // true
+// console.log(4 <= 2);      // false
+
+
+
+// var age = prompt("Enter your age baby")
+
+// if ( age >= 18){
+//    console.log("You are eligible for CNIC");
+   
+// } else {
+//    console.log("You are not eligible for CNIC");
+   
+// }
+
+
+// var userAge = prompt("Enter your age:");
+// if (userAge == 18) {
+//   console.log("Welcome to adulthood!");    // only check value not a data typr
+// }
+
+
+
+// var gender = prompt("Enter gender (M/F):");
+// if (gender == "M") {
+//   console.log("Male selected");
+// }else {
+//    console.log("Female Selected");
+   
+// }
+
+ 
+var theme = "1"; // from database as string
+if (theme == 1) {
+  console.log("Light mode activated");
+}
+
+
+
