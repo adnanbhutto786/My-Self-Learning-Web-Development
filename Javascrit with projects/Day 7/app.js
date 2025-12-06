7a809d3c40bef97104c11bddec5d1661c9c1380b
Adnan Ali Bhutto <adnan2292707@gmail.com>
2025-12-06 09:03:26 +0500

    Add app.js with basic loop examples
    Introduced app.js containing commented JavaScript examples for table printing, counting, reverse counting, odd numbers, factorial calculation, reverse table, and sum within a range. Updated index.html to load app.js instead of script.js for demonstration purposes.

---
+++ Javascrit with projects/Day 7/app.js
@@ -0,0 +1,148 @@
+console.log("Hello World,");
+
+//table print using prompt
+
+
+// let num= prompt("Enter the number")
+// num=parseInt(num);
+
+// for ( let i = 1 ; i <=10 ; i++){
+//    console.log(num + " x " + i + " = " + (num * i));
+   
+// }
+
+// let num = prompt("Enter table num");
+// num = parseInt(num);
+
+// for ( let i = 1 ; i <= 10 ; i++){
+//    console.log( num + " X " + i + " = " + (num * i));
+   
+// }
+
+// counting 1 to N
+
+// let limitNum = prompt ( "Enter the Num");
+// limitNum = parseInt(limitNum);
+
+// for ( let i = 1 ; i <= limitNum ; i++){
+//    console.log(i);
+   
+// }
+// 4
+
+// let limitNum = prompt("Enter the count no");
+// limitNum = parseInt(limitNum);
+
+// for ( let i = 1 ; i <= limitNum ; i++){
+//    console.log(i);
+   
+// }
+
+
+/// reverse counting N say 1...
+
+
+
+
+// let reverseCounting = prompt("Enter number");
+// reverseCounting = parseInt(reverseCounting);
+
+// for ( let i = reverseCounting ; i >=1 ; i--){
+//    console.log(i);
+   
+// }
+
+// let reverseCounting = prompt("Enter number");
+// reverseCounting = parseInt(reverseCounting);
+
+
+// for ( let i = reverseCounting ; i >=1 ; i--){
+//    console.log(i);
+   
+// }
+
+
+// Odd number print...
+
+
+// let oddNum = prompt ("Enter number");
+// oddNum = parseInt(oddNum
+// )
+//  for ( let i = 1 ; i<= oddNum; i++){
+//    if ( i % 2 !==0){
+//       console.log(i);
+      
+//    }
+
+//  }
+
+
+
+
+// let oddNum = prompt ("Enter number");
+// oddNum = parseInt ( oddNum);
+
+
+// for ( let i = 1 ; i <= oddNum ; i++){
+//    if ( i % 2 !==0){
+//       console.log(i);
+      
+//    }
+// }
+
+
+//  user say num lo factorial calculate kro..
+
+// let factNum = prompt("Enter number");
+// factNum = parseInt ( factNum);
+// let fact = 1;
+
+// for ( let i = 1 ; i <= factNum ; i++){
+//    fact = fact * i;
+// }
+
+// console.log("Factorial: " , fact);
+
+
+// user say no aur reverse oder me table print kroo..
+
+
+// let num = prompt("Enter number");
+// num = parseInt ( num)
+
+// for ( let i = 10 ; i >= 1 ; i--){
+//    console.log(num + " x " + i + " = " + (num * i));
+   
+// }
+
+
+// start and end num ka sum lo with in loop
+
+
+// let start = prompt("Enter Start num")
+// start = parseInt(start)
+
+// let end = prompt("Enter End num")
+// end = parseInt(end)
+
+// let totalSum = 0 ;
+
+
+// for ( let i = start ; i <= end ; i++){
+//    totalSum = totalSum + i
+// }
+
+// console.log("Sum of:",totalSum);
+
+
+
+
+
+
+
+
+
+
+
+
+
